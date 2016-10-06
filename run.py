419e5cda0c11119d17119ead2b0467745f586a00
Ran <lr_naril@163.com>
2016-10-06 13:06:23 +0800

    some change

--- run.py
+++ run.py
@@ -5,4 +5,4 @@ from website import app
 if __name__ == '__main__':
     app.debug = True
     Scss(app, static_dir='static', asset_dir='assets')
-    app.run(port=4001, debug=True)
+    app.run(port=4002, debug=True)
